c781282c0029358630321cd3491b10fa8cfc75e5
Thiago Henrique Poiani <thpoiani@gmail.com>
2016-05-03 09:59:42 -0300

    OpenLayers 3

--- src/js/controllers/HomeController.js
+++ src/js/controllers/HomeController.js
@@ -1,4 +1,4 @@
-;(function(angular) {
+;(function(angular, ol, undefined) {
     'use strict';
 
     angular
@@ -6,7 +6,22 @@
         .controller('HomeController', HomeController);
 
     function HomeController($log) {
-        $log.debug('HomeController')
+        $log.debug('HomeController');
+
+        var map = new ol.Map({
+          target: 'map',
+          layers: [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            })
+          ],
+          view: new ol.View({
+            center: ol.proj.fromLonLat([-47.890926, -22.008708]),
+            zoom: 15,
+            maxZoom: 19,
+            minZoom: 4
+          })
+        });
     }
 
-}(window.angular));
+}(window.angular, window.ol));
