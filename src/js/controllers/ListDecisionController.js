3f9d8a6bec1dbf4ba06964ad5c76dae148f47565
Flavio Horita <feahorita@gmail.com>
2016-08-07 17:10:47 -0300

    Implemented remove feature for decisions

--- src/js/controllers/ListDecisionController.js
+++ src/js/controllers/ListDecisionController.js
@@ -5,20 +5,40 @@
 	angular.module('agora-geodash')
 			.controller('ListDecisionController', ListDecisionController);
 
-	function ListDecisionController($scope, $http, $log) {
+	function list($scope, $http) {
+		$http.get('http://localhost:8080/decision/list')
+    	.then(function (res) {
+    		console.log("resposta!");
+    		$scope.decisions = res.data; 
+    	})
+    	.catch(function (err) {
+    		console.log(err);
+    	});
+	}
+	
+	function ListDecisionController($scope, $http, $log, $location) {
 		$log.debug('ListDecisionController');
 		
 		$scope.decisions = [];
 		
-		$http.get('http://localhost:8080/decision/list')
-	    	.then(function (res) {
-	    		console.log("resposta!");
-	    		$scope.decisions = res.data; 
-	    	})
-	    	.catch(function (err) {
-	    		console.log(err);
-	    	});
+		list($scope, $http);
 		
+		$scope.delete = function (id) {
+			console.log(id);
+			
+			$http({
+	            url: 'http://localhost:8080/decision/delete'
+	                , method: 'DELETE'
+	                , data: id
+	                , header: {'content-type':'application/json'}
+	            }).success(function (response) {
+	                console.log('success', response);
+                	list($scope, $http);
+	            }).error(function (error) {
+	                console.log('error'+error);
+	            });
+			
+		}
 	}
 
 }(window.angular, window.ol));
