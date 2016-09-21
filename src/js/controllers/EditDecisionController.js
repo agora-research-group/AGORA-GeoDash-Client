5f6d0928a6c5521d8c6ec4015d4d802ab92042d3
Flavio Horita <feahorita@gmail.com>
2016-09-21 05:00:37 -0300

    Included a field for relating information requirements to decisions

--- src/js/controllers/EditDecisionController.js
+++ src/js/controllers/EditDecisionController.js
@@ -5,11 +5,49 @@
 	angular.module('agora-geodash')
 			.controller('EditDecisionController', EditDecisionController);
 
+	function listInfoReqs($scope, $http) {
+		$http.get('http://localhost:8080/informationRequirement/list')
+    	.then(function (res) {
+    		console.log("resposta!");
+    		$scope.infReqs = res.data; 
+    	})
+    	.catch(function (err) {
+    		console.log(err);
+    	});
+	}
+	
+	function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(infReqs) {
+    	var lowertitle = infReqs.title.toLowerCase();
+    	var lowerdescription = infReqs.description.toLowerCase();  
+    	  
+        return (lowertitle.indexOf(lowercaseQuery) === 0) ||
+            (lowerdescription.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+	
 	function EditDecisionController($scope, $http, $log, $location, $routeParams) {
 		$log.debug('EditDecisionController');
 		
 		$scope.decision={id:null,title:'',description:''};
 		$scope.paramId = $routeParams.id;
+		
+		$scope.infReqs = [];
+		$scope.selectedInfReqs = [];
+		$scope.selectedItem = null;
+		$scope.searchText = null;
+		
+		$scope.decision={id:null,title:'',description:''};
+
+		listInfoReqs($scope, $http);
+		
+		$scope.querySearch = function(query) {
+	      var results = query ? $scope.infReqs.filter(createFilterFor(query)) : [];
+	      return results;
+	    };
 		
 		$http({
             url: 'http://localhost:8080/decision/find'
@@ -18,15 +56,19 @@
                 , header: {'content-type':'application/json'}
             }).success(function (response) {
             	$scope.decision = response;
+            	$scope.selectedInfReqs = $scope.decision.infReqs;
             }).error(function (error) {
                 console.log('error'+error);
             });
 		
 		$scope.register = function() {
+			var decision = $scope.decision;
+			decision.infReqs = $scope.selectedInfReqs;
+			
 			$http({
 	            url: 'http://localhost:8080/decision/save'
 	                , method: 'POST'
-	                , data: $scope.decision
+	                , data: decision
 	                , header: {'content-type':'application/json'}
 	            }).success(function (response) {
 	                console.log('success', response);
@@ -34,7 +76,7 @@
 	                console.log('error'+error);
 	            });
 			
-			$location.path("/ListDecisions");
+			$location.path("ListDecisions");
 		}
 	}
 
--- src/js/controllers/NewDecisionController.js
+++ src/js/controllers/NewDecisionController.js
@@ -5,16 +5,55 @@
 	angular.module('agora-geodash')
 			.controller('NewDecisionController', NewDecisionController);
 
+	function listInfoReqs($scope, $http) {
+		$http.get('http://localhost:8080/informationRequirement/list')
+    	.then(function (res) {
+    		console.log("resposta!");
+    		$scope.infReqs = res.data; 
+    	})
+    	.catch(function (err) {
+    		console.log(err);
+    	});
+	}
+	
+	function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(infReqs) {
+    	var lowertitle = infReqs.title.toLowerCase();
+    	var lowerdescription = infReqs.description.toLowerCase();  
+    	  
+        return (lowertitle.indexOf(lowercaseQuery) === 0) ||
+            (lowerdescription.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+	
 	function NewDecisionController($scope, $http, $log, $location) {
 		$log.debug('NewDecisionController');
 		
+		$scope.infReqs = [];
+		$scope.selectedInfReqs= [];
+		$scope.selectedItem = null;
+		$scope.searchText = null;
+		
 		$scope.decision={id:null,title:'',description:''};
+
+		listInfoReqs($scope, $http);
+		
+		$scope.querySearch = function(query) {
+	      var results = query ? $scope.infReqs.filter(createFilterFor(query)) : [];
+	      return results;
+	    };
 		
 		$scope.register = function() {
+			var decision = $scope.decision;
+			decision.infReqs = $scope.selectedInfReqs;
+			
 			$http({
 	            url: 'http://localhost:8080/decision/save'
 	                , method: 'POST'
-	                , data: $scope.decision
+	                , data: decision
 	                , header: {'content-type':'application/json'}
 	            }).success(function (response) {
 	                console.log('success', response);
@@ -22,7 +61,7 @@
 	                console.log('error'+error);
 	            });
 			
-			$location.path("/ListDecisions");
+			$location.path("ListDecisions");
 		}
 	}
 
