215e5fe11811923740aaa8a6e5de956dab41173d
Flavio Horita <feahorita@gmail.com>
2016-11-17 08:24:46 -0200

    Included accumulate value in the property

--- src/js/controllers/HomeController.js
+++ src/js/controllers/HomeController.js
@@ -49,19 +49,6 @@
     	});
 	}
 
-	var WORLD_DIM = { height: 256, width: 256 };
-    var ZOOM_MAX = 21;
-
-    function latRad(lat) {
-        var sin = Math.sin(lat * Math.PI / 180);
-        var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-        return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-    }
-
-    function zoom(mapPx, worldPx, fraction) {
-        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-    }
-
 	function HomeController($scope, $http, $log, cfpLoadingBar, $mdDialog) {
 		$log.debug('HomeController');
 		
